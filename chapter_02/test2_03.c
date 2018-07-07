6837df3d01e651433607d1cbe11bbdbc963409b8
KingSuo <648230502@qq.com>
2018-07-07 11:05:42 +0800

    fix test2_03.c

--- chapter_02/test2_03.c
+++ chapter_02/test2_03.c
@@ -4,16 +4,23 @@ int htoi (char s[]);
 
 int main(int argc, char const *argv[])
 {
-	
+	printf("%d\n", htoi("0xFF"));
 	return 0;
 }
 
 int htoi (char s[]) {
-	int i, value;
+	int i, value, temp;
 	value = 0;
 
-	for (i = 2; s[i] != '\0'; ++i)
-		value = value * 16 + s[i];
+	for (i = 2; (temp = s[i]) != '\0'; ++i) {
+		if (temp >= '0' && temp <= '9')
+			temp = (int) temp - 48;
+		if (temp >= 'a' && temp <= 'f')
+			temp = (int) temp - 87;
+		if (temp >= 'A' && temp <= 'F')
+			temp = (int) temp - 55;
+		value = value * 16 + temp;
+	}
 
 	return value;
 }
