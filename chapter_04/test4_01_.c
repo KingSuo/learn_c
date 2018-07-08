51a8aeabb9d8ff105fd4a3f18807de7dd7ab6045
KingSuo <648230502@qq.com>
2018-07-08 15:29:34 +0800

    add test4_01_.c

---
+++ chapter_04/test4_01_.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <string.h>
+
+int strrindex(char s[], char t[]);
+
+int main(int argc, char const *argv[])
+{
+	char s[] = "asdfghjkl";
+	char t[] = "fgh";
+	printf("%d\n", strrindex(s, t));
+	return 0;
+}
+
+int strrindex(char s[], char t[]) {
+	int i, j, k;
+
+	for (i = strlen(s) - 1; i >= strlen(t) - 1; i--) {
+		j = strlen(t);
+		for (k = i; j >= 0 && s[k] == t[j]; k--, j--);
+		if (j < 0)
+			return k;
+	}
+	return -1;
+}
